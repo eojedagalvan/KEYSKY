ba5af765a81e99b14b541eae69559ea1aaadab9a
eojedagalvan <eduardo.ojeda@edu.uag.mx>
2021-03-28 22:08:19 -0600

    inicio.php

--- php/inicio.php
+++ php/inicio.php
@@ -28,7 +28,7 @@
         <nav class="menu">
           <img src="../images/KEYSKY 4.jpg" alt="logo">
         <ul>
-          <li><a href="#">Inicio</a></li>
+          <li><a href="inicio.php">Inicio</a></li>
           <li><a href="#">Mis reservaciones</a></li>
           <li><a href="#">Acerca de KEYSKY</a></li>
           <li class="drop"><a><?php echo $_SESSION['Nombre'] ; echo " " ;echo $_SESSION['Apellido']?></a>
@@ -41,5 +41,43 @@
         </ul>
       </nav>
     </header>
+    <section>
+      <div class="slider">
+        <ul>
+          <li><img src="../images/slider/1.jpg" alt=""></li>
+          <li><img src="../images/slider/2.jpg" alt=""></li>
+          <li><img src="../images/slider/3.jpg" alt=""></li>
+          <li><img src="../images/slider/4.jpg" alt=""></li>
+          <li><img src="../images/slider/5.jpg" alt=""></li>
+          <li><img src="../images/slider/6.jpg" alt=""></li>
+        </ul>
+        <div class="fondo">
+        </div>
+        <div class="slider-text">
+          <h2>Bienvenido a</h2>
+          <h1>KEYSKY</h1>
+        </div>
+      </div>
+    </section>
+    <section class="busqueda">
+      <img src="../images/inicio/1.jpg" alt="inicio">
+      <form class="" action="index.html" method="post">
+        <h1>Busca alojamientos en KEYSKY</h1>
+        <p>Descubre alojamientos enteros y habitaciones
+           privadas, perfectos para cualquier viaje.</p>
+           <label for="ubicacion">Ubicación</label>
+             <select class="opciones lugares" name="">
+               <option value="">Lugares</option>
+                <option value="">Tapalpa</option>
+             </select>
+          <label for="llegada">Llegada</label>
+          <input type="date" name="llegada" value="" class="opciones fecha" min="<?php echo date("Y-m-d");?>">
+          <br>
+          <label for="salida">Salida</label>
+          <br>
+          <input type="date" name="salida" value="" class="opciones fecha" min="<?php echo date("Y-m-d");?>">
+          <input type="submit" name="" value="Buscar" class="submit">
+      </form>
+    </section>
   </body>
 </html>
